4f4844e76db771e6213765d7986fbba6764aa569
Joshua De Matas <joshua.de.matas@lendo.se>
2026-02-08 14:01:14 +0100

    feat: created simple backend

---
+++ backend/api/routes.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/blood-vessel/basex/assert"
+	"github.com/charmbracelet/log"
+	"github.com/spf13/viper"
+
+	"github.com/labstack/echo/v4"
+)
+
+func registerRoutes(
+	e *echo.Echo,
+	logger *log.Logger,
+	config *viper.Viper,
+	deps *ServerDependencies,
+) {
+	assert.AssertNotNil(e)
+	assert.AssertNotNil(logger)
+	assert.AssertNotNil(deps)
+	assert.AssertNotNil(config)
+
+	e.GET("", func(c echo.Context) error {
+		logger.Debug("basex")
+		return c.String(http.StatusOK, "basex")
+	})
+}
+
+func bindAndValidate[T any](c echo.Context) (*T, error) {
+	var req T
+	err := c.Bind(&req)
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	err = c.Validate(req)
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	return &req, nil
+}
---
+++ backend/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/blood-vessel/basex/api"
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	config := viper.New()
+	config.AutomaticEnv()
+
+	var envPath string
+	flag.StringVar(&envPath, "env", "", "path to .env file to use")
+	flag.Parse()
+
+	var err error
+	if envPath != "" {
+		err = godotenv.Load(envPath)
+	} else {
+		err = godotenv.Load()
+	}
+	if err != nil {
+		fmt.Printf("failed to load .env file %s\n", err)
+	}
+
+	config.SetDefault("PORT", 8080)
+	port := config.GetInt("PORT")
+	log.Info("listening", "port", port)
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal("failed to listen", "err", err)
+	}
+
+	ctx := context.Background()
+	opts := &api.RunOptions{
+		Writer:   os.Stdout,
+		Listener: ln,
+		Config:   config,
+	}
+	if err := api.Run(ctx, opts); err != nil {
+		log.Fatal("failed to run", "err", err)
+	}
+}
